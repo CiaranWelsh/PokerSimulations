3497ae2377689c8d7403e068e56fdc9d68b0021b
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2019-04-07 22:46:41 +0100

    added travis, setup.py modified layout of repo, now ready for coding

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PokerSimulations',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/CiaranWelsh/PokerSimulations',
+    license='GPL',
+    author='Ciaran Welsh',
+    author_email='ciaran.welsh@newcastle.ac.uk',
+    description='Poker Simulations and Analysis'
+)
